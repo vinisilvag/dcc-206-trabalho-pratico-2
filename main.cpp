f1f4362076e0b8f3cb360c3f7f4d392639589783
Vinicius Gomes <vinicius.svgomes@gmail.com>
2022-05-29 10:13:37 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// 2021421869 - Vinicius Silva Gomes
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+
+#define f first
+#define s second
+#define pb push_back
+
+int main() { _
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
